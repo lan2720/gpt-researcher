46b7cacc847fe052111b6a78528cb19793473377
jarvixwang <jarvixwang@futunn.com>
2023-10-16 18:41:07 +0800

    梳理清楚整个ResearchAgent的workflow

--- agent/prompts.py
+++ agent/prompts.py
@@ -37,7 +37,7 @@ def generate_search_queries_prompt(question):
     Returns: str: The search queries prompt for the given question
     """
 
-    return f'Write 3 google search queries to search online that form an objective opinion from the following: "{question}"'\
+    return f'Write 3 google search queries to search online that form an objective opinion from the following: "{question}".\n'\
            f'Use the current date if needed: {datetime.now().strftime("%B %d, %Y")}.\n' \
            f'You must respond with a list of strings in the following format: ["query 1", "query 2", "query 3"].'
 
